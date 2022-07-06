4313f29e142c95e4526ff4afb328e7fe3ffa8df6
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-05 21:25:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,5 @@
   <p>Johan</p>
   <p></p>
   <object data="fluxograma.pdf" height="100%" width="100%"></object>
-  <a href="https://igorbecker2.herokuapp.com/" download="fluxograma.pdf">Baixe!</a>
  </body>
 </html>
